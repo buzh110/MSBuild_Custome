861a1b433af4878ae68b1c3fad0fba4b46920ce0
buzh110 <15574986542@163.com>
2017-04-11 18:15:52 +0800

    需要文件比较

--- MSBuild_Custome/MainWindow.xaml.cs
+++ MSBuild_Custome/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,40 +23,58 @@ namespace MSBuild_Custome
     /// </summary>
     public partial class MainWindow : Window
         {
-        string installedPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Make-EXE\Make-EXE.exe";
         public MainWindow()
             {
             InitializeComponent();
             }
-
+      
         private void Button_Click(object sender, RoutedEventArgs e)
             {
+            new Thread(() => {
             //项目路径
-            string PackagePath = tbProjectPath.Text;
+             var PackagePath = @"F:\WorkPlace\DigitalBook\DigitalBook.sln";//tbProjectPath.Text;
             //编译Msbuild地址
-            string MSBuildPath = tbMSBPath.Text;
-
-          // var psi = new ProcessStartInfo("cmd.exe", String.Format(".\{0} {1} /p:Configuration=Release", MSBuildPath,PackagePath));
+                string currentdir = System.IO.Path.Combine( Environment.CurrentDirectory , "Output");
+                string commands = string.Format(@"{0}  /p:Configuration=Release,OutDir={1}  /flp1:logfile=errors.txt;errorsonly", PackagePath, currentdir);
+                Directory.Delete(currentdir,true);
             Process p = new Process();
-            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.FileName = @"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe";
             p.StartInfo.UseShellExecute = false;
+                p.StartInfo.Arguments = commands;
             p.StartInfo.RedirectStandardInput = true;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.RedirectStandardError = true;
-            p.StartInfo.CreateNoWindow = false;
+            p.StartInfo.CreateNoWindow = true;
+      
             p.Start();
+            p.OutputDataReceived += sortProcess_OutputDataReceived;
+            p.ErrorDataReceived += errorProcess_OutputDataReceived;
             string strOutput = null;
-            //   p.StandardInput.WriteLine("cd D:\\flv\\mplayer");
-            //   p.StandardInput.WriteLine("cd d:");
-            // p.StandardInput.WriteLine(string.Format("D:\\flv\\mplayer\\mencoder \"c:\\vs.wmv\" -o \"c:\\output.flv\" -of lavf  -lavfopts i_certify_that_my_video_stream_does_not_use_b_frames -oac mp3lame -lameopts abr:br=56 -ovc lavc -lavcopts vcodec=flv:vbitrate={0}:mbd=2:mv0:trell:v4mv:cbp:last_pred=3:dia=4:cmp=6:vb_strategy=1 -vf scale=512:-3 -ofps 12 -srate 22050", 200));
-            p.StandardInput.WriteLine(@"c: ");  //先转到系统盘下
-            p.StandardInput.WriteLine(@"cd: C:\Program Files (x86)\MSBuild\14.0\Bin");  //先转到系统盘下
-            p.StandardInput.WriteLine("exit");
-            strOutput = p.StandardOutput.ReadToEnd();
-            Console.WriteLine(strOutput);
+            p.StandardInput.AutoFlush = true;
+
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
             p.WaitForExit();
-            
+                
+
             p.Close();
-        }
+            }).Start();
+            }
+
+         private void sortProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+            {
+            if (!String.IsNullOrEmpty(e.Data))
+                {
+                Dispatcher.Invoke(new Action(() => { this.lbshow.Items.Add(e.Data); lbshow.ScrollIntoView(e.Data); }));
+                }
+            }
+        //错误信息
+        private void errorProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+            {
+            if (!String.IsNullOrEmpty(e.Data))
+                {
+                Dispatcher.Invoke(new Action(() => { this.lbshow.Items.Add(e.Data); lbshow.ScrollIntoView(e.Data); }));
+                }
+            }
         }
     }
