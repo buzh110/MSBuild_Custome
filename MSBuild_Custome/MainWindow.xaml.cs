4d6b4601c1afdc5887e788b2ca1c66b08ad73a13
buzh110 <15574986542@163.com>
2017-04-14 18:12:38 +0800

    xml文件部分

--- MSBuild_Custome/MainWindow.xaml.cs
+++ MSBuild_Custome/MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Path = System.IO.Path;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace MSBuild_Custome
     {
@@ -27,22 +30,78 @@ namespace MSBuild_Custome
         public MainWindow()
             {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
             }
+
+        XmlDocument xmldoc;
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+            {
+            tbProjectPath.Text = @"F:\WorkPlace\DigitalBook\DigitalBook.sln";
+            tbMSBPath.Text = @"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe";
+
+          
+            }
+
+        /// <summary>
+        /// 初始化xml文件
+        /// </summary>
+        /// <returns>返回文件路径</returns>
+        private  string InitXml(string filePath)
+            {
+            var xmlpath = filePath;
+            XmlHelper.CreateXmlDocument(xmlpath, "root", "1.0", "utf-8", "yes");
+            //打包配置节点
+            XmlHelper.CreateOrUpdateXmlNodeByXPath(xmlpath, "root", "XmlModelHead", "");
+            //ms编译节点
+            XmlHelper.CreateOrUpdateXmlNodeByXPath(xmlpath, "root", "XmlPreMode", "");
+            //文件对照节点
+            XmlHelper.CreateOrUpdateXmlNodeByXPath(xmlpath, "root", "XmlFilesMode", "");
+            return xmlpath;
+            }
+
         static AutoResetEvent mEvent = new AutoResetEvent(false);
+        /// <summary>
+        /// 编译按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
             {
-
-
-         Thread msThread=   new Thread(() => {
-          
-             //项目路径
-             var PackagePath = @"F:\WorkPlace\DigitalBook\DigitalBook.sln";//tbProjectPath.Text;
-            //编译Msbuild地址
-             //   string currentdir = System.IO.Path.Combine( Environment.CurrentDirectory , "Output");
-                string commands = string.Format(@"{0} /t:Build /p:TargetFramework=v4.0 /p:Configuration=Release  /flp1:logfile=errors.txt;errorsonly", PackagePath);
-                //Directory.Delete(currentdir,true);
+            //生成配置文件名称
+            if (string.IsNullOrEmpty(tbPackage.Text)) { tbPackage.Text = string.Format("{0}/{1}{2}", Environment.CurrentDirectory, tbPackageName.Text, ".xml"); }
+            if (!Directory.Exists(tbPackage.Text)) InitXml(tbPackage.Text);
+            XElement xe = XElement.Load(tbPackage.Text);
+            var list = from s in xe.Elements("XmlPreMode") select s;
+            if (list.Count()>0)
+                {
+                XElement first = list.First();
+                          ///替换新的节点
+           first.ReplaceNodes(
+                       new XElement("MSBuildPath", tbMSBPath.Text),
+                    new XElement("ProjectPath", tbProjectPath),
+                   //  new XElement("OutPutPath", (double)dgvBookInfo.CurrentRow.Cells[4].Value),
+                     new XElement("FilterStrings", @"^.+\.(pdb)|(log)$")
+                       );
+                xe.Save(tbPackage.Text);
+                }
+            else
+                {
+                XElement record = new XElement(
+            new XElement("XmlPreMode",
+             new XElement("MSBuildPath", tbMSBPath.Text),
+                    new XElement("ProjectPath", tbProjectPath),
+                     //  new XElement("OutPutPath", (double)dgvBookInfo.CurrentRow.Cells[4].Value),
+                     new XElement("FilterStrings", @"^.+\.(pdb)|(log)$")));
+                           xe.Add(record);
+                           xe.Save(tbPackage.Text);
+                }
+            var projectPath = tbProjectPath.Text;//tbProjectPath.Text;
+            var msbuildPath = tbMSBPath.Text;
+            Thread msThread=   new Thread(() => {
+                string commands = string.Format(@"{0} /t:Build /p:TargetFramework=v4.0 /p:Configuration=Release  /flp1:logfile=errors.txt;errorsonly", projectPath);
             Process p = new Process();
-            p.StartInfo.FileName = @"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe";
+            p.StartInfo.FileName = msbuildPath;
             p.StartInfo.UseShellExecute = false;
                 p.StartInfo.Arguments = commands;
             p.StartInfo.RedirectStandardInput = true;
@@ -85,12 +144,6 @@ namespace MSBuild_Custome
             FileHelper.CopyDirectory(foldpath, OutPutPath, @"^.+\.(pdb)|(log)$");
 
             var filelist = FileHelper.GetFileNames(OutPutPath, "*", true);
-            //var list = FileHelper.GetFileNames(OutPutPath).Where(s => s.EndsWith(".pdb") || s.Contains(".log"));
-         
-            //foreach (var item in list)
-            //    {
-            //    FileHelper.DeleteFile(item);
-            //    }
             }
 
         private void sortProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
@@ -111,10 +164,6 @@ namespace MSBuild_Custome
 
         private void btPackage_Click(object sender, RoutedEventArgs e)
             {
-            //Dispatcher.Invoke(() => {
-            //    lbshow.Items.;
-            //});
-            //打包
             new Thread(()=> {
               var  INNOSCRIPTFILE = "C:\\Users\\yinji\\Desktop\\test.iss";
                 string Argument = string.Format("/q \"{0}\"", INNOSCRIPTFILE);
@@ -142,6 +191,58 @@ namespace MSBuild_Custome
                 p.Close();
               
             }).Start();
+            }
+
+        /// <summary>
+        /// 打包项目文件（xml）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbPackage_MouseDown(object sender, MouseButtonEventArgs e)
+            {
+            string filePath = GetFilePath("(*.xml)");
+            if (!string.IsNullOrEmpty(filePath)) {
+                tbPackage.Text = filePath; }
+            }
+        /// <summary>
+        /// 项目地址
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbProjectPath_MouseDown(object sender, MouseButtonEventArgs e)
+            {
+            string filePath = GetFilePath("(*.sln,*.project)|*.sln;*.project");
+            if (!string.IsNullOrEmpty(filePath)) tbProjectPath.Text = filePath;
+            }
+
+        private string GetFilePath(string filter)
+            {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            string filePath = "";
+            ofd.Filter = filter;
+            ofd.FileOk += (m, s) => {
+                filePath = ofd.FileName;
+            };
+            ofd.ShowDialog();
+            return filePath;
+            }
+
+        private void tbPackage_GotFocus(object sender, RoutedEventArgs e)
+            {
+            string filePath = GetFilePath("(*.xml)|*.xml|All files(*.*)|*.*");
+            if (!string.IsNullOrEmpty(filePath)) {
+                tbPackage.Text = filePath;
+                //文件存在 读取文件并赋值
+                if (File.Exists(filePath))
+                    {
+                    XElement xe = XElement.Load(filePath);
+                    var se = from s in xe.Elements("XmlPreMode") select s.FirstNode;
+                    
+                 //   tbMSBPath.Text = se.ToList().Element("MSBuildPath").Value;
+                    //tbProjectPath.Text = se.Element("ProjectPath").Value;
+                    }
+                        }
             }
         }
     }
